0812b22ba23f71d18d83747d9345512ed9097800
Dominique McCloud <niquelejon@gmail.com>
2017-10-28 00:00:44 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,12 +4,54 @@ require 'csv'
     @songs = Song.all
   end
 
+  def show
+   @song = Song.find(params[:id])
+ end
+
+ def new
+   @song = Song.new
+ end
+
+ def create
+   @song = Song.new(song_params)
+
+   if @song.save
+     redirect_to @song
+   else
+     render :new
+   end
+ end
+
   def upload
     CSV.foreach(params[:file].path, headers: true) do |song|
       Song.create(title: lead[0], artist_name: song[1])
     end
     redirect_to songs_path
   end
+
+  def edit
+  @song = Song.find(params[:id])
+end
+
+def update
+  @song = Song.find(params[:id])
+
+  @song.update(song_params)
+
+  if @song.save
+    redirect_to @song
+  else
+    render :edit
+  end
+end
+
+def destroy
+  @song = Song.find(params[:id])
+  @song.destroy
+  flash[:notice] = "Song deleted."
+  redirect_to songs_path
+end
+
 
 
   private
